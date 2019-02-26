a6862010444643b97917f657d1fc3b9b135c5117
Sean Nam <skyun.nam@gmail.com>
2019-02-26 23:06:19 +0900

    Update Project Information and Files

---
+++ projects/guestbook/guestbook.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE TABLE guestbook (
+  no        NUMBER,
+  name      VARCHAR2(80),
+  password  VARCHAR2(20),
+  content   VARCHAR2(2000),
+  reg_date  DATE,
+  PRIMARY KEY(no)
+);
+
+CREATE SEQUENCE seq_guestbook_no
+INCREMENT BY 1 
+START WITH 1 ;
