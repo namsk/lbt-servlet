730c8ba7883497417f2fba0ba5cadfc35a4c738d
Sean Nam <skyun.nam@gmail.com>
2022-02-08 17:20:30 +0900

    Update Project

---
+++ projects/guestbook/guestbook.mysql.sql
@@ -0,0 +1,9 @@
+﻿
+CREATE TABLE guestbook (
+  no        INT PRIMARY KEY AUTO_INCREMENT,
+  name      VARCHAR(80),
+  password  VARCHAR(20),
+  content   VARCHAR(2000),
+  reg_date  DATETIME NOT NULL DEFAULT NOW()
+);
+
